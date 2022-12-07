e26f1c8492d7d28414c66bfd69a21a7366f3ed13
IlieChrisitan <cristi_ilie99@yahoo.com>
2022-12-07 17:25:14 +0200

    .

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48,57);
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
 
-window.addEventListener('load', () => {
-    registerSW();
-});
+// window.addEventListener('load', () => {
+//     registerSW();
+// });
 
-async function registerSW() {
-    if ('serviceWorker' in navigator) {
-        try {
-            await navigator.serviceWorker.register('sw.js');
-        } catch (e) {
-            console.log('SW registration failed');
-        }
-    }
-};
+// async function registerSW() {
+//     if ('serviceWorker' in navigator) {
+//         try {
+//             await navigator.serviceWorker.register('sw.js');
+//         } catch (e) {
+//             console.log('SW registration failed');
+//         }
+//     }
+// };
 
 
 form.addEventListener('submit', () => {
