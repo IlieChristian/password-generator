58664e466d1105bda706a7f96e0a3c4bb3eff42a
Ilie Christian <89945490+IlieChristian@users.noreply.github.com>
2023-01-31 18:46:15 +0200

    .

--- script.js
+++ script.js
@@ -150,3 +150,17 @@ characterAmountNumber.ontouchend = (() => {
         inputValue.classList.remove("show");
     }, 600);
 });
+
+window.addEventListener('load', () => {
+    registerSW();
+});
+
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+      try {
+        await navigator.serviceWorker.register('./sw.js');
+      } catch (e) {
+        console.log(`SW registration failed`);
+      }
+    }
+  }
